440f4de992c4aec4c5ba3709618fccb384a32a78
lichun <2747517125@qq.com>
2025-02-16 21:00:09 +0800

    feat-站点配置-查询接口

--- api/site_api/enter.go
+++ api/site_api/enter.go
@@ -2,14 +2,53 @@ package site_api
 
 import (
 	"blogx/common/res"
+	"blogx/global"
+	"blogx/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 type SiteApi struct{}
 
+type SiteInfoRequest struct {
+	Name string `uri:"name"`
+}
+
 func (SiteApi) SiteInfoView(c *gin.Context) {
-	res.OkWithData("xx", c)
+	var cr SiteInfoRequest
+	err := c.ShouldBindUri(&cr)
+	if err != nil {
+		res.FailWithError(err, c)
+		return
+	}
+
+	if cr.Name == "site" {
+		res.OkWithData(global.Conf.Site, c)
+		return
+	}
+
+	// 判断角色是不是管理员
+	middleware.Admin(c)
+	_, ok := c.Get("claims")
+	if !ok {
+		return
+	}
+
+	var data any
+	switch cr.Name {
+	case "email":
+		data = global.Conf.Email
+	case "qq":
+		data = global.Conf.QQ
+	case "qiNiu":
+		data = global.Conf.QiNiu
+	case "ai":
+		data = global.Conf.Ai
+	default:
+		res.FailWithMsg("不存在的配置", c)
+		return
+	}
+	res.OkWithData(data, c)
 }
 
 type SiteUpdateRequest struct {
--- router/site.go
+++ router/site.go
@@ -9,6 +9,6 @@ import (
 
 func SiteRouter(r *gin.RouterGroup) {
 	app := api.App.Site
-	r.GET("site", app.SiteInfoView)
+	r.GET("site/:name", app.SiteInfoView)
 	r.PUT("site", middleware.Admin, app.SiteUpdateView)
 }
